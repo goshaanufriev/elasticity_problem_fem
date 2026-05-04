ec966764c14eb8a3e66f828d57a2fb7298d93796
goshaanufriev <g.anufriev25@yandex.ru>
2026-05-05 00:28:11 +0300

    coded FEM till the end, solved linear system with conjugate gradients (needs checking)

--- fem_elast/fem_elast.cpp
+++ fem_elast/fem_elast.cpp
@@ -18,6 +18,7 @@ struct FiniteElement
 {
 	std::vector<Node> nodes;
 	std::vector<int> numNodes;
+	int type{}; // 0 - L, 1 - U
 };
 
 std::vector<Node> Grid(Segment& xSeg, Segment& ySeg, int& nx, int& ny)
@@ -75,18 +76,187 @@ int Delta(int& i, int& j)
 	return 0;
 }
 
+double cubeNorm(std::vector<std::vector<double>>& A, const int& n)
+{
+	double norm = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		double s = 0;
+		for (int j = 0; j < n; ++j)
+		{
+			s += std::fabs(A[i][j]);
+		}
+		if (s > norm)
+			norm = s;
+	}
+	return norm;
+}
+
+double cubeNorm(std::vector<double>& b, const int& n)
+{
+	double norm = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (std::fabs(b[i]) > norm)
+			norm = std::fabs(b[i]);
+	}
+	return norm;
+}
+
+std::vector<double> multMat(const std::vector<std::vector<double>>& A, const std::vector<double>& B)
+{
+	int n = B.size();
+	std::vector<double> y(n);
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			y[i] += A[i][j] * B[j];
+		}
+	}
+	return y;
+}
+
+std::vector<double> addVec(const std::vector<double>& x, const std::vector<double>& y)
+{
+	int n = x.size();
+	std::vector <double> z(n);
+	for (int i = 0; i < n; ++i)
+	{
+		z[i] = x[i] + y[i];
+	}
+	return z;
+}
+
+std::vector<double> subVec(const std::vector<double>& x, const std::vector<double>& y)
+{
+	int n = x.size();
+	std::vector <double> z(n);
+	for (int i = 0; i < n; ++i)
+	{
+		z[i] = x[i] - y[i];
+	}
+	return z;
+}
+
+//std::vector<double> Jacobi(std::vector<std::vector<double>>& A, std::vector<double>& b, const int& n)
+//{
+//	std::vector<std::vector<double>> C(n, std::vector<double>(n));
+//	std::vector<double> y(n);
+//	
+//	for (int i = 0; i < n; ++i)
+//	{
+//		for (int j = 0; j < n; ++j)
+//		{
+//			if (i == j)
+//			{
+//				C[i][j] = 0;
+//				y[i] = b[i] / A[i][j];
+//			}
+//			else
+//			{
+//				C[i][j] = -A[i][j] / A[i][i];
+//			}
+//		}
+//	}
+//
+//	std::vector<double> x0(n);
+//	for (int i = 0; i < n; ++i)
+//	{
+//		x0[i] = 0;
+//	}
+//
+//	double eps = 1e-6;
+//	double normC = cubeNorm(C, n);
+//
+//	std::vector<double> cx = multMat(C, x0, n);
+//	std::vector<double> x = addVec(cx, y, n);
+//	std::vector<double> err = subVec(x, x0, n);
+//	double errNorm = cubeNorm(err, n);
+//	std::cout << errNorm << "\n";
+//	while (errNorm > eps)
+//	{
+//		x0 = x;
+//		cx = multMat(C, x0, n);
+//		x = addVec(cx, y, n);
+//		err = subVec(x, x0, n);
+//		errNorm = cubeNorm(err, n);
+//		std::cout << errNorm << "\n";
+//	}
+//	return x;
+//}
+
+std::vector<double> kVec(std::vector<double>& v, double& k)
+{
+	std::vector<double> u(v.size());
+	for (int i = 0; i < u.size(); ++i)
+	{
+		u[i] = k * v[i];
+	}
+	return u;
+}
+
+std::vector<double> conjGrad(std::vector<std::vector<double>>& A, std::vector<double>& b, std::vector<double>& x0)
+{
+	int n = b.size();
+	double alpha = 0;
+	std::vector<double> ax = multMat(A, x0);
+	std::vector<double> r = subVec(ax, b);
+	std::vector<double> ar = multMat(A, r);
+	double beta = -dot(r, r) / dot(ar, r);
+	std::vector<double> delta = kVec(r, beta);
+	std::vector<double> x = addVec(x0, delta);
+	std::vector<double> adx, dalpha, rbeta;
+	for (int i = 1; i < n; ++i)
+	{
+		ax = multMat(A, x);
+		r = subVec(ax, b);
+		ar = multMat(A, r);
+		adx = multMat(A, delta);
+		// dots
+		double rr = dot(r, r);
+		double ardx = dot(ar, delta);
+		double rdx = dot(r, delta);
+		double arr = dot(ar, r);
+		double adxdx = dot(adx, delta);
+
+		double denom = adxdx * arr - ardx * ardx;
+		double alpha = (rr * ardx - rdx * arr) / denom;
+		double beta = (rdx * ardx - rr * adxdx) / denom;
+		dalpha = kVec(delta, alpha);
+		rbeta = kVec(r, beta);
+		delta = addVec(dalpha, rbeta);
+		x = addVec(x, delta);
+	}
+	return x;
+}
+
 std::vector<double> rhs1()
 {
 	return { 0, -10 };
+}
+
+struct Coeffs
+{
+	double a, b, c;
+};
+
+std::vector<double> uEx(double& x, double& y, Coeffs& uxCoeff, Coeffs& uyCoeff)
+{
+	double a1 = uxCoeff.a, a2 = uxCoeff.b, a3 = uxCoeff.c;
+	double b1 = uyCoeff.a, b2 = uyCoeff.b, b3 = uyCoeff.c;
+	double ux = a1 * x * x + a2 * x * y + a3 * y * y;
+	double uy = b1 * x * x + b2 * x * y + b3 * y * y;
+	return { ux, uy };
 }
 
 int main()
 {
 	// Генерация сетки
 	Segment xSeg{ 0, 10 };
-	int nx = 100;
+	int nx = 30;
 	Segment ySeg{ 0, 5 };
-	int ny = 100;
+	int ny = 20;
 	std::vector<Node> grid = Grid(xSeg, ySeg, nx, ny);
 
 	// Константы
@@ -105,8 +275,10 @@ int main()
 			FiniteElement feL, feU;
 			feU.nodes = { grid[inds[0]], grid[inds[1]], grid[inds[2]] };
 			feU.numNodes = { inds[0], inds[1], inds[2] };
+			feU.type = 1;
 			feL.nodes = { grid[inds[0]], grid[inds[2]], grid[inds[3]] };
 			feL.numNodes = { inds[0], inds[2], inds[3] };
+			feL.type = 0;
 			finEls.push_back(feU);
 			finEls.push_back(feL);
 		}
@@ -135,14 +307,14 @@ int main()
 		// Формирование локальной матрицы жёсткости
 		std::vector<std::vector<double>> B(3, std::vector<double>(6));
 		// верхнетреугольный КЭ
-		if (elem.numNodes[2] - elem.numNodes[1] == 1)
+		if (elem.type == 1)
 		{
 			for (int i = 0; i < 3; ++i)
 			{
 				B[0][2 * i] = bU[i];
 				B[1][2 * i + 1] = cU[i];
-				B[2][2 * i] = cU[i] / sqrt(2);
-				B[2][2 * i + 1] = bU[i] / sqrt(2);
+				B[2][2 * i] = cU[i];
+				B[2][2 * i + 1] = bU[i];
 			}
 		}
 		// нижнетреугольный КЭ
@@ -152,8 +324,8 @@ int main()
 			{
 				B[0][2 * i] = bL[i];
 				B[1][2 * i + 1] = cL[i];
-				B[2][2 * i] = cL[i] / sqrt(2);
-				B[2][2 * i + 1] = bL[i] / sqrt(2);
+				B[2][2 * i] = cL[i];
+				B[2][2 * i + 1] = bL[i];
 			}
 		}
 		std::vector<std::vector<double>> Ke(6, std::vector<double>(6));
@@ -198,6 +370,106 @@ int main()
 		}
 	}
 
+	// Учёт ГУ 1 рода
+	// y = 0: (a1*x^2, b1*x^2)
+	// x = lx: (a1*lx^2+a2*lx*y+a3*y^2, b1*lx^2+b2*lx*y+b3*y^2)
+	// y = ly: (a1*x^2+a2*x*ly+a3*ly^2, b1*x^2+b2*x*ly+b3*ly^2)
+	// x = 0: (ux=a3*y^2, uy=b3*y^2)
+
+	double a1 = 0;
+	double a3 = 1;
+	double b1 = 1;
+	double b3 = 0;
+	double f1 = 0;
+	double f2 = -1;
+
+	double a2 = -(2 * mu * b1 + (2 * lambda + 4 * mu) * b3 + f2) / (lambda + mu);
+	double b2 = -(2 * mu * a3 + (2 * lambda + 4 * mu) * a1 + f1) / (lambda + mu);
+
+	double eps = 1e-10;
+	for (int i = 0; i < gSize; ++i)
+	{
+		double x = grid[i].x;
+		double y = grid[i].y;
+
+		int I = 2 * i;
+		int J = 2 * i + 1;
+
+		double ux, uy;
+		bool flag = false;
+		if (std::fabs(y - ySeg.a) < eps)
+		{
+			ux = a1 * x * x;
+			uy = b1 * x * x;
+			flag = true;
+		}
+		else if (std::fabs(x - xSeg.b) < eps)
+		{
+			ux = a1 * xSeg.b * xSeg.b + a2 * xSeg.b * y + a3 * y * y;
+			uy = b1 * xSeg.b * xSeg.b + b2 * xSeg.b * y + b3 * y * y;
+			flag = true;
+		}
+		else if (std::fabs(y - ySeg.b) < eps)
+		{
+			ux = a1 * x * x + a2 * ySeg.b * x + a3 * ySeg.b * ySeg.b;
+			uy = b1 * x * x + b2 * ySeg.b * x + b3 * ySeg.b * ySeg.b;
+			flag = true;
+		}
+		else if (std::fabs(x - xSeg.a) < eps)
+		{
+			ux = a3 * y * y;
+			uy = b3 * y * y;
+			flag = true;
+		}
+
+		if (flag)
+		{
+			// ux
+			for (int j = 0; j < 2 * gSize; ++j)
+			{
+				F[j] -= K[j][I] * ux;
+				K[I][j] = 0;
+				K[j][I] = 0;
+			}
+			K[I][I] = 1;
+			F[I] = ux;
+
+			// uy
+			for (int j = 0; j < 2 * gSize; ++j)
+			{
+				F[j] -= K[j][J] * uy;
+				K[J][j] = 0;
+				K[j][J] = 0;
+			}
+			K[J][J] = 1;
+			F[J] = uy;
+		}
+	}
+
+	int sysSize = 2 * gSize;
+	std::vector<double> u0(sysSize);
+	std::vector<double> u = conjGrad(K, F, u0);
+
+	Coeffs uxCoef{ a1, a2, a3 };
+	Coeffs uyCoef{ b1, b2, b3 };
+
+	double normDelta = 0, normU = 0;
+	for (int i = 0; i < gSize; ++i)
+	{
+		double x = grid[i].x;
+		double y = grid[i].y;
+		std::vector<double> uE = uEx(x, y, uxCoef, uyCoef);
+		double ux = u[2 * i];
+		double uy = u[2 * i + 1];
+		double uN = sqrt(uE[0] * uE[0] + uE[1] * uE[1]);
+		if (uN > normU)
+			normU = uN;
+		double r = sqrt(pow(ux - uE[0], 2) + pow(uy - uE[1], 2));
+		if (r > normDelta)
+			normDelta = r;
+	}
+	double delta = normDelta / normU;
+	std::cout << delta;
 
 	return 0;
 }
